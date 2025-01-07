656b850abffac4b37fe4ac2924ca6fd63b0d786d
aleev97 <valverdealejandro327@gmail.com>
2025-01-06 21:35:13 -0300

    responsive

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -37,46 +37,46 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <div className="p-4">
-      <h1 className="text-5xl font-extrabold text-center mb-20 leading-tight tracking-wide 
-              bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent 
-              bg-clip-text shadow-xl shadow-black-500/50 relative">
+    <div className="p-4 sm:p-6 lg:p-8">
+      <h1 className="text-3xl sm:text-5xl font-extrabold text-center mb-6 sm:mb-10 leading-tight tracking-wide 
+            bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent 
+            bg-clip-text shadow-xl shadow-black-500/50 relative flex items-center justify-center">
         Lista de Tareas
-        <span className="text-black absolute right-96 top-1/3 transform -translate-y-1/2 text-5xl">📝</span>
+        <span className="text-black ml-3 text-3xl sm:text-5xl relative -top-1.5">📝</span>
       </h1>
-      <div className="text-center flex justify-center space-x-8">
+      <div className="flex justify-center items-center mt-4 sm:mt-4">
         <button
           onClick={() => setIsEditorOpen(true)}
-          className="mb-3 p-3 text-white border-2 border-transparent rounded-md bg-gradient-to-r from-purple-600 to-purple-700 hover:border-purple-400 hover:border-3 font-semibold text-lg w-48 h-12 transition-all ease-in-out transform hover:scale-110 shadow-lg flex items-center justify-center"
+          className="p-3 text-white border-2 border-transparent rounded-full bg-gradient-to-r from-purple-600 to-purple-700 hover:border-purple-400 font-semibold text-lg sm:text-xl w-48 sm:w-60 h-12 transition-all ease-in-out transform hover:scale-110 shadow-lg flex items-center justify-center"
         >
           Crear nueva tarea
         </button>
       </div>
-      <div className="absolute top-20 right-4 mt-4 mr-4 mb-2 flex flex-col items-center">
-        <label htmlFor="filter" className="block text-l font-semibold text-gray-900 text-center">
+      <div className="flex flex-col items-center sm:items-start sm:absolute sm:top-24 sm:right-10 mt-4">
+        <label htmlFor="filter" className="block text-sm sm:text-base font-semibold text-gray-900 text-center">
           Filtrar tareas por estado
         </label>
         <select
           id="filter"
           value={filter}
           onChange={handleFilterChange}
-          className="mt-1 block w-36 px-3 py-1.5 bg-violet-200 border border-stone-950 rounded-lg shadow-sm text-center text-s focus:ring-slate-900 focus:border-teal-700"
+          className="mt-2 block w-36 sm:w-48 px-3 py-1.5 bg-violet-200 border border-stone-950 rounded-lg shadow-sm text-center text-sm sm:text-base focus:ring-slate-900 focus:border-teal-700"
         >
           <option value="all">Todas</option>
           <option value="completed">Completadas</option>
           <option value="pending">Pendientes</option>
         </select>
       </div>
-      <TaskList onEdit={handleEdit} filter={filter} />
-      
+      <div className="mt-6 sm:mt-8">
+        <TaskList onEdit={handleEdit} filter={filter} />
+      </div>
       {isEditorOpen && (
-        // Modal container with overlay background
-        <div className=" backdrop-blur-sm fixed inset-0 bg-gray-200 bg-opacity-50 flex justify-center items-center z-50">
-          <div className="p-6 rounded-lg w-full max-w-2xl">
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-50 backdrop-blur-sm z-50">
+          <div className="p-6 rounded-lg w-full max-w-md sm:max-w-lg lg:max-w-2xl">
             <TaskEditor
               taskToEdit={editingTask || { id: "", content: "", color: "", font: "", textColor: "" }}
               onSave={handleCloseEditor}
-              onCancel={handleCloseEditor} // Prop to cancel
+              onCancel={handleCloseEditor}
             />
           </div>
         </div>
